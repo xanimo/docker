5f081fd1e5837aef465b313591dcdd1d8cd50634
AbcSxyZ <AbcSxyZ@users.noreply.github.com>
2021-12-04 15:53:33 +0100

    Add test coverage for Dockerfile

---
+++ tests/entrypoint_hook.py
@@ -0,0 +1,120 @@
+import sys
+import os
+import json
+import difflib
+import entrypoint
+
+class CommandNotFound(Exception):
+    """Raised when entrypoint command is not found or test fail."""
+
+class Command:
+    """
+    Represent a single execve command, with
+    its arguments and environment.
+
+    Can represent an entrypoint hooked command, the expected
+    result or the input of a test.
+    """
+    def __init__(self, argv, environ):
+        #sort cli arguments to facilitate comparaison
+        sorted_args = argv[1:]
+        sorted_args.sort()
+        self.argv = [argv[0]] + sorted_args
+        self.environ = environ
+
+    def __eq__(self, other):
+        """Compare 2 Command, result of a test and expected command."""
+        return self.argv == other.argv and self.environ == other.environ
+
+    def stringify(self):
+        """Render single command into string for error outputs."""
+        argv_str = json.dumps(self.argv, indent=4)
+        command_str = f"argv: {argv_str}\n"
+        environ_str = json.dumps(self.environ, indent=4)
+        command_str += f"environ: {environ_str}"
+        return command_str
+
+    def __repr__(self):
+        """Override pytest assert message error"""
+        return "<420UselessMessage420>"
+
+class EntrypointHook:
+    """
+    Hook to perform test of the entrypoint.
+    
+    Hook os.execve function to catch launched command.
+    Manage all information about test input, test output and
+    expected output.
+    """
+    def __init__(self):
+        self._reset_attributes()
+
+        #Set up execve hook globally
+        os.execve = self._execve_hook
+
+    def entrypoint(self, test_argv, test_environ):
+        """
+        Run a command using entrypoint. Can be used manually
+        if no need to compare command launched by entrypoint.
+
+        Fake entrypoint arguments (sys.argv) and environment variables.
+        """
+        #Clean hook from previous test, store the command to test.
+        self._reset_attributes()
+        self.input = Command(test_argv, test_environ)
+
+        #Manage system arguments & environment used by the script
+        sys.argv[1:] = test_argv.copy()
+        os.environ = test_environ.copy()
+
+        #Launch entrypoint script
+        entrypoint.main()
+
+        if self.result is None:
+            raise CommandNotFound("Test fail, do not return a command")
+
+    def test(self, test_argv, test_environ, \
+            result_argv, result_environ):
+        """
+        Run a test of entrypoint and store expected result in the hook
+        for further comparaison.
+        """
+        self.entrypoint(test_argv, test_environ)
+        self.reference = Command(result_argv, result_environ)
+
+    def error_msg(self):
+        """Error message to display on assert failure"""
+        msg = "\n======= Input ======= \n"
+        msg += self.input.stringify() + "\n"
+        msg += "\n======= Result ======= \n"
+        msg += self.result.stringify() + "\n"
+        msg += "======= Expected ======= \n"
+        msg += self.reference.stringify()
+        msg += "======= Diff ======= \n"
+        msg += self._diff()
+        return msg
+
+    def _execve_hook(self, executable, argv, environ):
+        """
+        Hook for os.execve function, to catch arguments/environment
+        instead of launching processes.
+        """
+        self.result = Command(argv, environ)
+
+    def _reset_attributes(self):
+        """Clean state between each test"""
+        self.result = None
+        self.reference = None
+        self.input = None
+
+    def _diff(self):
+        """Perform diff between test result and the expected result."""
+        diff_tool = difflib.Differ()
+        result_content = self.result.stringify().splitlines()
+        reference_content = self.reference.stringify().splitlines()
+        result = diff_tool.compare(result_content, reference_content)
+        return "\n".join(result)
+
+    def __repr__(self):
+        """Override pytest assert message error"""
+        return "<69UselessMessage69>"
---
+++ tests/test_dockerfile.py
@@ -0,0 +1,393 @@
+import sys
+import os
+import pytest
+from entrypoint_hook import EntrypointHook, CommandNotFound
+
+EXECUTABLES_FOLDER = "/usr/local/bin/"
+HOOK = EntrypointHook()
+
+def abs_path(executable):
+    """Build manually (expected) executable absolute path"""
+    return os.path.join(EXECUTABLES_FOLDER, executable)
+
+def test_entrypoint_executables():
+    """
+    Basic test without configuration to check
+    if entrypoint run each dogecoin executables.
+    """
+    ## Constant variable for test
+    test_environ = {
+        "DATADIR" : "/dogecoin/.dogecoin",
+        "USER" : "dogecoin",
+        "PATH" : os.environ["PATH"],
+            }
+
+    result_environ = {
+        "USER" : "dogecoin",
+        "PATH" : os.environ["PATH"],
+            }
+
+    ## Test basic command with `dogecoind`
+    test_args = ["dogecoind"]
+
+    result_args = [
+            abs_path("dogecoind"),
+            "-datadir=/dogecoin/.dogecoin",
+            "-printtoconsole",
+            ]
+    HOOK.test(test_args, test_environ, result_args, result_environ)
+    assert HOOK.result == HOOK.reference, HOOK.error_msg()
+    
+    ## Test empty command with `dogecoin-qt`
+    test_args = ["dogecoin-qt"]
+
+    result_args = [
+            abs_path("dogecoin-qt"),
+            "-datadir=/dogecoin/.dogecoin",
+            "-printtoconsole",
+            ]
+
+    HOOK.test(test_args, test_environ, result_args, result_environ)
+    assert HOOK.result == HOOK.reference, HOOK.error_msg()
+
+    ## Test empty command with `dogecoin-cli`
+    test_args = ["dogecoin-cli"]
+
+    result_args = [
+            abs_path("dogecoin-cli"),
+            "-datadir=/dogecoin/.dogecoin",
+            ]
+
+    HOOK.test(test_args, test_environ, result_args, result_environ)
+    assert HOOK.result == HOOK.reference, HOOK.error_msg()
+
+    ## Test basic command with `dogecoin-tx`
+    tx_result_env = {
+        "USER" : "dogecoin",
+        "PATH" : os.environ["PATH"],
+        "DATADIR" : "/dogecoin/.dogecoin",
+            }
+
+    test_args = ["dogecoin-tx"]
+
+    result_args = [
+            abs_path("dogecoin-tx"),
+            ]
+    HOOK.test(test_args, test_environ, result_args, tx_result_env)
+    assert HOOK.result == HOOK.reference, HOOK.error_msg()
+
+def test_invalid_command():
+    test_args = ["dogecoindx"]
+
+    with pytest.raises(CommandNotFound):
+        HOOK.entrypoint(test_args, os.environ)
+
+def test_environ():
+    """
+    Verify if environment is converted to arguments,
+    control that arguments are removed from the environment.
+    """
+    #Control environment variables with values
+    test_args = ["dogecoind"]
+    test_env = {
+            "USER" : "dogecoin",
+            "PATH" : os.environ['PATH'],
+            "DATADIR" : "/dogecoin/.dogecoin",
+            "MAXCONNECTIONS" : "150",
+            "PAYTXFEE" : "0.01"
+            }
+
+    result_args = [
+            abs_path("dogecoind"),
+            "-datadir=/dogecoin/.dogecoin",
+            "-paytxfee=0.01",
+            "-maxconnections=150",
+            "-printtoconsole",
+            ]
+    result_env = {
+            "USER" : "dogecoin",
+            "PATH" : os.environ['PATH'],
+            }
+    HOOK.test(test_args, test_env, result_args, result_env)
+    assert HOOK.result == HOOK.reference, HOOK.error_msg()
+
+    #Control environment variables with empty values
+    test_env = {
+            "USER" : "dogecoin",
+            "PATH" : os.environ['PATH'],
+            "DATADIR" : "/dogecoin/.dogecoin",
+            "TESTNET" : "",
+            "DAEMON" : "",
+            }
+
+    result_args = [
+            abs_path("dogecoind"),
+            "-daemon",
+            "-datadir=/dogecoin/.dogecoin",
+            "-testnet",
+            "-printtoconsole",
+            ]
+    result_env = {
+            "USER" : "dogecoin",
+            "PATH" : os.environ['PATH'],
+            }
+    HOOK.test(test_args, test_env, result_args, result_env)
+    assert HOOK.result == HOOK.reference, HOOK.error_msg()
+
+def test_arguments():
+    """Verifying arguments are being kept appropriatly"""
+    #Verify arguments with values
+    test_args = ["dogecoind", "-maxconnections=150", "-paytxfee=0.01"]
+    test_env = {
+            "USER" : "dogecoin",
+            "PATH" : os.environ['PATH'],
+            "DATADIR" : "/dogecoin/.dogecoin",
+            }
+
+    result_args = [
+            abs_path("dogecoind"),
+            "-datadir=/dogecoin/.dogecoin",
+            "-maxconnections=150",
+            "-paytxfee=0.01",
+            "-printtoconsole",
+            ]
+    result_env = {
+            "USER" : "dogecoin",
+            "PATH" : os.environ['PATH'],
+            }
+    HOOK.test(test_args, test_env, result_args, result_env)
+    assert HOOK.result == HOOK.reference, HOOK.error_msg()
+
+    #Verify arguments without values
+    test_args = ["dogecoind", "-daemon", "-testnet"]
+    test_env = {
+            "USER" : "dogecoin",
+            "PATH" : os.environ['PATH'],
+            "DATADIR" : "/dogecoin/.dogecoin",
+            }
+
+    result_args = [
+            abs_path("dogecoind"),
+            "-datadir=/dogecoin/.dogecoin",
+            "-daemon",
+            "-testnet",
+            "-printtoconsole",
+            ]
+    result_env = {
+            "USER" : "dogecoin",
+            "PATH" : os.environ['PATH'],
+            }
+    HOOK.test(test_args, test_env, result_args, result_env)
+    assert HOOK.result == HOOK.reference, HOOK.error_msg()
+
+    #Mixing arguments with and without values
+    test_args = ["dogecoind", "-daemon", "-maxconnections=150"]
+    test_env = {
+            "USER" : "dogecoin",
+            "PATH" : os.environ['PATH'],
+            "DATADIR" : "/dogecoin/.dogecoin",
+            }
+
+    result_args = [
+            abs_path("dogecoind"),
+            "-datadir=/dogecoin/.dogecoin",
+            "-daemon",
+            "-maxconnections=150",
+            "-printtoconsole",
+            ]
+    result_env = {
+            "USER" : "dogecoin",
+            "PATH" : os.environ['PATH'],
+            }
+    HOOK.test(test_args, test_env, result_args, result_env)
+    assert HOOK.result == HOOK.reference, HOOK.error_msg()
+
+def test_arguments_double_dash():
+    """Check arguments formates with double-dash like `--testnet`"""
+    test_args = ["dogecoind", "--maxconnections=150", "--paytxfee=0.01"]
+    test_env = {
+            "USER" : "dogecoin",
+            "PATH" : os.environ['PATH'],
+            "DATADIR" : "/dogecoin/.dogecoin",
+            }
+
+    result_args = [
+            abs_path("dogecoind"),
+            "-datadir=/dogecoin/.dogecoin",
+            "--maxconnections=150",
+            "--paytxfee=0.01",
+            "-printtoconsole",
+            ]
+    result_env = {
+            "USER" : "dogecoin",
+            "PATH" : os.environ['PATH'],
+            }
+    HOOK.test(test_args, test_env, result_args, result_env)
+    assert HOOK.result == HOOK.reference, HOOK.error_msg()
+
+def test_mixing_argument_and_env():
+    """Configure container with arguments and environment variables"""
+    test_args = ["dogecoind", "-maxconnections=150", "-daemon"]
+    test_env = {
+            "USER" : "dogecoin",
+            "PATH" : os.environ['PATH'],
+            "DATADIR" : "/dogecoin/.dogecoin",
+            "TESTNET" : "",
+            }
+
+    result_args = [
+            abs_path("dogecoind"),
+            "-datadir=/dogecoin/.dogecoin",
+            "-maxconnections=150",
+            "-daemon",
+            "-testnet",
+            "-printtoconsole",
+            ]
+    result_env = {
+            "USER" : "dogecoin",
+            "PATH" : os.environ['PATH'],
+            }
+    HOOK.test(test_args, test_env, result_args, result_env)
+    assert HOOK.result == HOOK.reference, HOOK.error_msg()
+
+def test_equal_argv_and_env():
+    """Check arguments and environment with identical variables"""
+    test_args = ["dogecoind", "-maxconnections=150", "-daemon"]
+    test_env = {
+            "USER" : "dogecoin",
+            "PATH" : os.environ['PATH'],
+            "DATADIR" : "/dogecoin/.dogecoin",
+            "MAXCONNECTIONS" : "150",
+            "DAEMON" : "",
+            }
+
+    result_args = [
+            abs_path("dogecoind"),
+            "-datadir=/dogecoin/.dogecoin",
+            "-maxconnections=150",
+            "-maxconnections=150",
+            "-daemon",
+            "-daemon",
+            "-printtoconsole",
+            ]
+    result_env = {
+            "USER" : "dogecoin",
+            "PATH" : os.environ['PATH'],
+            }
+    HOOK.test(test_args, test_env, result_args, result_env)
+    assert HOOK.result == HOOK.reference, HOOK.error_msg()
+
+    #Same variable with different value for env & arguments.
+    test_args = ["dogecoind", "-maxconnections=130", "-daemon"]
+    test_env = {
+            "USER" : "dogecoin",
+            "PATH" : os.environ['PATH'],
+            "DATADIR" : "/dogecoin/.dogecoin",
+            "MAXCONNECTIONS" : "150",
+            "DAEMON" : "1",
+            }
+
+    result_args = [
+            abs_path("dogecoind"),
+            "-datadir=/dogecoin/.dogecoin",
+            "-maxconnections=130",
+            "-maxconnections=150",
+            "-daemon",
+            "-daemon=1",
+            "-printtoconsole",
+            ]
+    result_env = {
+            "USER" : "dogecoin",
+            "PATH" : os.environ['PATH'],
+            }
+    HOOK.test(test_args, test_env, result_args, result_env)
+    assert HOOK.result == HOOK.reference, HOOK.error_msg()
+
+def test_help_debug():
+    """
+    Test option with dash like `-help-debug` if working
+    properly in environment.
+    """
+    test_args = ["dogecoind"]
+    test_env = {
+            "USER" : "dogecoin",
+            "PATH" : os.environ['PATH'],
+            "DATADIR" : "/dogecoin/.dogecoin",
+            "HELP_DEBUG" : "",
+            }
+
+    result_args = [
+            abs_path("dogecoind"),
+            "-datadir=/dogecoin/.dogecoin",
+            "-help-debug",
+            "-printtoconsole",
+            ]
+    result_env = {
+            "USER" : "dogecoin",
+            "PATH" : os.environ['PATH'],
+            }
+    HOOK.test(test_args, test_env, result_args, result_env)
+    assert HOOK.result == HOOK.reference, HOOK.error_msg()
+
+def test_files_metadata(host):
+    """Verify all image files generated by the Dockerfile"""
+    dogecoind = host.file(abs_path("dogecoind"))
+    assert dogecoind.user == "dogecoin"
+    assert dogecoind.group == "dogecoin"
+    assert dogecoind.mode == 0o4555
+
+    dogecoinqt = host.file(abs_path("dogecoin-qt"))
+    assert dogecoinqt.user == "dogecoin"
+    assert dogecoinqt.group == "dogecoin"
+    assert dogecoinqt.mode == 0o4555
+
+    dogecointx = host.file(abs_path("dogecoin-tx"))
+    assert dogecointx.user == "dogecoin"
+    assert dogecointx.group == "dogecoin"
+    assert dogecointx.mode == 0o4555
+
+    dogecoincli = host.file(abs_path("dogecoin-cli"))
+    assert dogecoincli.user == "dogecoin"
+    assert dogecoincli.group == "dogecoin"
+    assert dogecoincli.mode == 0o4555
+
+    entrypoint_script = host.file(abs_path("entrypoint.py"))
+    assert entrypoint_script.user == "root"
+    assert entrypoint_script.group == "root"
+    assert entrypoint_script.mode == 0o500
+
+def test_datadir(host):
+    """
+    Verify if datadir can be changed and created proprely.
+
+    Verify datadir metada, if it's owned by script user (dogecoin).
+    """
+    #Use a unique directory for this test
+    test_datadir = "/tmp/datadir_test"
+    datadir_argument = f"-datadir={test_datadir}"
+
+    test_args = ["dogecoind", datadir_argument]
+    test_environ = {
+        "USER" : "dogecoin",
+        "PATH" : os.environ["PATH"],
+            }
+
+    result_args = [
+            abs_path("dogecoind"),
+            datadir_argument,
+            "-printtoconsole",
+            ]
+    result_environ = {
+        "USER" : "dogecoin",
+        "PATH" : os.environ["PATH"],
+            }
+
+    #Run test to generate datadir
+    HOOK.test(test_args, test_environ, result_args, result_environ)
+    assert HOOK.result == HOOK.reference, HOOK.error_msg()
+
+    #Test datadir metadata
+    datadir_folder = host.file(test_datadir)
+    assert datadir_folder.user == "dogecoin"
+    assert datadir_folder.group == "dogecoin"
+    assert datadir_folder.mode == 0o755
